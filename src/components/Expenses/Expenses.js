fc9a6e05834175cb95cfdcbcc11b79cfbad3af5c
sgno28 <s.ngotuantu@gmail.com>
2022-09-25 23:52:50 +0100

    commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.expense[0].title}
+        amount={props.expense[0].amount}
+        date={props.expense[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expense[1].title}
+        amount={props.expense[1].amount}
+        date={props.expense[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expense[2].title}
+        amount={props.expense[2].amount}
+        date={props.expense[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expense[3].title}
+        amount={props.expense[3].amount}
+        date={props.expense[3].date}
+      ></ExpenseItem>
+    </Card>
+  );
+}
+
+export default Expenses;
