7203647790c4d98f9d45b0cb913fd0a91faefb99
sgno28 <s.ngotuantu@gmail.com>
2022-10-06 11:33:54 +0100

    laptop commit

--- src/App.js
+++ src/App.js
@@ -24,10 +24,14 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+  const AddExpenseHandler = expense => {
+    console.log("In App.js");
+    console.log(expense);
+  };
 
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={AddExpenseHandler}/>
       <Expenses expense={expenses} />
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,39 +1,97 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   // Set States
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate] = useState('');
-  
+
+  // Multistate approach
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  // Grouping the states
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: ''
+  // });
+
   // Event Handlers to update states
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
+    // Use this method of state updating when
+    // your current state depends on your previous states
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value};
+    // });
   };
 
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.taget.value,
+    // })
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.taget.value,
+    // })
+  };
+
+  const submitHandler = (event) => {
+    // On form onclick, default behaviour is to refresh page
+    event.preventDefault();
+
+    // Constructing the expense data object since we are using multistate
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    // Accesses the function from new expense
+    // To pass data up to the parent component
+    // Using props and passing a function as a prop
+    props.onFormSubmit(expenseData);
+    // Reset form
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle} // two-way-binding to reset form after submission
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount} // two-way-binding to reset form after submission
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2022-09-01" max="2023-12-31" onChange={dateChangeHandler} />
+          <input
+            type="date"
+            min="2022-09-01"
+            max="2023-12-31"
+            value={enteredDate} // two-way-binding to reset form after submission
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
